30f33e11a0ed5dba0febae0254f2e73f0d578737
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2025-05-26 14:25:04 +0200

    feat: Add auth routes and swagger

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,8 @@ app.get('/api/health', (req, res) => {
 
 // Routes API
 app.use('/api/db', require('./routes/db'));
-// app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/docs', require('./routes/swagger'));
 // app.use('/api/menu', require('./routes/menu'));
 // app.use('/api/orders', require('./routes/orders'));
 // app.use('/api/restaurants', require('./routes/restaurants'));
