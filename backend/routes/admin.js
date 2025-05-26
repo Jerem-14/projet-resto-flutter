25e0f01dc92585efe8ad0dd928fc61420fc40797
Jerem-14 <jeremy.aubry69@gmail.com>
2025-05-26 18:15:09 +0200

    fix erreur api timeslots + swagger

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,6 +4,7 @@ const timeslotController = require('../controllers/timeslotController');
 const authenticate = require('../middleware/authenticate');
 
 // Routes pour les créneaux horaires
+router.get('/timeslots', authenticate, timeslotController.getAll);
 router.post('/timeslots', authenticate, timeslotController.create);
 router.patch('/timeslots/:id', authenticate, timeslotController.update);
 router.delete('/timeslots/:id', authenticate, timeslotController.delete);
